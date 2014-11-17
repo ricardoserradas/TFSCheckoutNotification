504eb27613729f71d6b2f6206c954cf16604a22a
unknown <ricardo.serradas@BRS-EST-014.brsolucoesintegradas.com.br>
2014-11-17 17:45:59 -0200

    rascunho da solução

---
+++ Solution/TfsCheckoutNotification.App/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TfsCheckoutNotification.App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menu_Configure = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer_checkInterval = new System.Windows.Forms.Timer(this.components);
+            this.contextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipTitle = "TFS Checkout Notification";
+            this.notifyIcon.ContextMenuStrip = this.contextMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "TFS Checkout Notification";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu_Configure,
+            this.menu_Exit});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.Size = new System.Drawing.Size(128, 48);
+            // 
+            // menu_Configure
+            // 
+            this.menu_Configure.Name = "menu_Configure";
+            this.menu_Configure.Size = new System.Drawing.Size(127, 22);
+            this.menu_Configure.Text = "Configure";
+            // 
+            // menu_Exit
+            // 
+            this.menu_Exit.Name = "menu_Exit";
+            this.menu_Exit.Size = new System.Drawing.Size(127, 22);
+            this.menu_Exit.Text = "Exit";
+            this.menu_Exit.Click += new System.EventHandler(this.menu_Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem menu_Configure;
+        private System.Windows.Forms.ToolStripMenuItem menu_Exit;
+        private System.Windows.Forms.Timer timer_checkInterval;
+    }
+}
+
---
+++ Solution/TfsCheckoutNotification.App/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TfsCheckoutNotification.App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.notifyIcon.BalloonTipText = string.Format("You have {0} pending checkin(s)", 2);
+            this.notifyIcon.ShowBalloonTip(3);
+        }
+
+        private void menu_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
