1180e5e8c5c2f17fb01aec273f54ba8500972428
ricardoserradas <ricardo.net@live.com>
2014-12-03 14:50:34 -0200

    adicionados os botões de checkin e undo

--- Solution/TfsCheckoutNotification.App/PendingChangesWindow.Designer.cs
+++ Solution/TfsCheckoutNotification.App/PendingChangesWindow.Designer.cs
@@ -31,15 +31,20 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PendingChangesWindow));
             this.lstPendingChanges = new System.Windows.Forms.CheckedListBox();
             this.lblPendingChanges = new System.Windows.Forms.Label();
+            this.btnCheckin = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lstPendingChanges
             // 
+            this.lstPendingChanges.CheckOnClick = true;
             this.lstPendingChanges.FormattingEnabled = true;
             this.lstPendingChanges.Location = new System.Drawing.Point(12, 27);
             this.lstPendingChanges.Name = "lstPendingChanges";
             this.lstPendingChanges.Size = new System.Drawing.Size(698, 229);
             this.lstPendingChanges.TabIndex = 0;
+            this.lstPendingChanges.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.lstPendingChanges_ItemCheck);
+            this.lstPendingChanges.SelectedValueChanged += new System.EventHandler(this.lstPendingChanges_SelectedValueChanged);
             // 
             // lblPendingChanges
             // 
@@ -50,11 +55,35 @@
             this.lblPendingChanges.TabIndex = 1;
             this.lblPendingChanges.Text = "All your pending changes are here:";
             // 
+            // btnCheckin
+            // 
+            this.btnCheckin.Enabled = false;
+            this.btnCheckin.Location = new System.Drawing.Point(716, 27);
+            this.btnCheckin.Name = "btnCheckin";
+            this.btnCheckin.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckin.TabIndex = 2;
+            this.btnCheckin.Text = "Check In";
+            this.btnCheckin.UseVisualStyleBackColor = true;
+            this.btnCheckin.Click += new System.EventHandler(this.btnCheckin_Click);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Enabled = false;
+            this.btnUndo.Location = new System.Drawing.Point(716, 56);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(75, 23);
+            this.btnUndo.TabIndex = 3;
+            this.btnUndo.Text = "Undo";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
             // PendingChangesWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(827, 274);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.btnCheckin);
             this.Controls.Add(this.lblPendingChanges);
             this.Controls.Add(this.lstPendingChanges);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -72,5 +101,7 @@
 
         private System.Windows.Forms.CheckedListBox lstPendingChanges;
         private System.Windows.Forms.Label lblPendingChanges;
+        private System.Windows.Forms.Button btnCheckin;
+        private System.Windows.Forms.Button btnUndo;
     }
 }
--- Solution/TfsCheckoutNotification.App/PendingChangesWindow.cs
+++ Solution/TfsCheckoutNotification.App/PendingChangesWindow.cs
@@ -18,9 +18,51 @@ namespace TfsCheckoutNotification.App
                 this.Close();
             }
 
+            PopulatePendingChangesList();
+        }
+
+        private void PopulatePendingChangesList()
+        {
+            if (this._mainForm.PendingChanges.Count <= 0) return;
+            lstPendingChanges.Items.Add("(Select  All)");
+
             foreach (var pendingChange in this._mainForm.PendingChanges)
             {
                 lstPendingChanges.Items.Add(pendingChange.ServerPath);
+            }
+        }
+
+        private void lstPendingChanges_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (e.Index != 0) return;
+            for (var i = 1; i < lstPendingChanges.Items.Count; i++)
+            {
+                lstPendingChanges.SetItemChecked(i, e.NewValue == CheckState.Checked);
+            }
+        }
+
+        private void btnCheckin_Click(object sender, System.EventArgs e)
+        {
+            if (
+                MessageBox.Show(
+                    "Are you sure you want to check in all the selected items?\r\n\r\nIf there's a check in policy for any of these pending changes, it will be overwritten.",
+                    "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                MessageBox.Show("Checkin here");
+            }
+        }
+
+        private void lstPendingChanges_SelectedValueChanged(object sender, System.EventArgs e)
+        {
+            btnCheckin.Enabled = lstPendingChanges.CheckedItems.Count > 0;
+            btnUndo.Enabled = lstPendingChanges.CheckedItems.Count > 0;
+        }
+
+        private void btnUndo_Click(object sender, System.EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to undo all the selected items?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                MessageBox.Show("Undo here.");
             }
         }
     }
